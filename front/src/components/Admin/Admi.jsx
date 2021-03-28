d793440ea366b3786dfe1cb601b1f9f161f6c335
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2021-03-28 03:50:09 +0200

    Create Admi.jsx

---
+++ front/src/components/Admin/Admi.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Admi extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Admi;
