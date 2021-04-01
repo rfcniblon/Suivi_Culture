bef5d5942386bc763a1ef1d3be07cc241ecdec06
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2021-04-01 05:08:34 +0200

    Update Navbar.jsx

--- front/src/components/Navbar/Navbar.jsx
+++ front/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 class NavbarAdmin extends Component {
 
@@ -12,7 +12,7 @@ class NavbarAdmin extends Component {
         return (
            <Fragment>
                <nav className="navbar navbar-expand-lg background">
-  <a className="navbar-brand titre_logo" href="/" title="bouton_home">Suivi de culture</a>
+  <a className="navbar-brand titre_logo" href="/" title="bouton_home">Suivi de potager</a>
   <button className="navbar-toggler" type="button" tiltle="bouton_navbar" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-icon"><i className="fas fa-bars"></i></span>
   </button>
@@ -20,15 +20,15 @@ class NavbarAdmin extends Component {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle a_navbar" href="#" id="navbarDropdown" tiltle="bouton_navigation" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a className="nav-link dropdown-toggle a_navbar" href="#/" id="navbarDropdown" tiltle="bouton_navigation" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i className="fas fa-user"></i>
         </a>
         <div className="dropdown-menu" id="dropdo" aria-labelledby="navbarDropdown">
-                    <label htmlFor="">Mon compte {user.user}</label>
-          <a className="dropdown-item a_navbar"  href="/configuration" title="configuration" >Configuration</a>
+                    <label className="text_deco">Mon compte </label><p>Bienvenue : {user.user}</p>
+          <a className="dropdown-item a_navbar"  href="/configuration" title="configuration"><i className="fas fa-wrench"></i> Configuration</a>
   
           <div className="dropdown-divider"></div>
-         <a className="dropdown-item a_navbar" href="/test" title="test" >Deconnexion</a>
+         <a className="dropdown-item a_navbar" href="/test" title="test" ><i className="fas fa-sign-out-alt"></i> Deconnexion</a>
         </div>
       </li>
       
