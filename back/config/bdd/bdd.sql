b8f7149f5873a740761167696b34cfdaea10fd0b
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2021-04-03 00:00:00 +0200

    Update bdd.sql

--- back/config/bdd/bdd.sql
+++ back/config/bdd/bdd.sql
@@ -9,68 +9,48 @@ CREATE TABLE `users` (
   `resetPasswordExpires` datetime DEFAULT NULL,
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
-  `roles` varchar(40) DEFAULT NULL,
-  `status` varchar(40) DEFAULT NULL,
+  `isverified` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `plante` (
-  `id_plante` int NOT NULL AUTO_INCREMENT,
-  `variete` varchar(255) DEFAULT NULL,
-  `quantite` varchar(255) DEFAULT NULL,
-  `origine` varchar(255) DEFAULT NULL,
-  `sexe` varchar(255) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id_plante`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `chambre_culture` (
-  `id_chambre` int NOT NULL AUTO_INCREMENT,
-  `fk_id_user` int DEFAULT NULL,
-  `fk_id_plante` int DEFAULT NULL,
-  `fonction` varchar(255) DEFAULT NULL,
-  `longueur` varchar(255) DEFAULT NULL,
-  `largeur` varchar(255) DEFAULT NULL,
-  `hauteur` varchar(255) DEFAULT NULL,
-  `surface` varchar(255) DEFAULT NULL,
-  `volume` varchar(255) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id_chambre`),
-  KEY `fk_id_plante` (`fk_id_plante`),
-  KEY `fk_id_user` (`fk_id_user`),
-  CONSTRAINT `chambre_culture_ibfk_1` FOREIGN KEY (`fk_id_plante`) REFERENCES `plante` (`id_plante`),
-  CONSTRAINT `chambre_culture_ibfk_2` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `fruit` (
-  `id_fruit` int NOT NULL AUTO_INCREMENT,
-  `origine` varchar(255) DEFAULT NULL,
-  `quantite` varchar(255) DEFAULT NULL,
-  `variete` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_fruit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `herbe` (
-  `id_herbe` int NOT NULL AUTO_INCREMENT,
-  `origine` varchar(100) DEFAULT NULL,
-  `quantite` varchar(255) DEFAULT NULL,
-  `sexe` varchar(255) DEFAULT NULL,
-  `variete` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_herbe`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `image` (
+CREATE TABLE `images` (
   `id_image` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `source` varchar(255) NOT NULL,
   PRIMARY KEY (`id_image`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `suivi` (
-  `id_suivi` int NOT NULL AUTO_INCREMENT,
-  `fk_id_plante` int DEFAULT NULL,
+CREATE TABLE `legumes` (
+  `id_legume` int NOT NULL AUTO_INCREMENT,
   `fk_id_image` int DEFAULT NULL,
-  `date` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `depart` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_legume`),
+  KEY `fk_id_image` (`fk_id_image`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `potagers` (
+  `id_potager` int NOT NULL AUTO_INCREMENT,
+  `fk_id_user` int DEFAULT NULL,
+  `fk_id_legume` int DEFAULT NULL,
+  `date` DATETIME DEFAULT NULL,
+  `fonction` varchar(255) DEFAULT NULL,
+  `longueur` varchar(255) DEFAULT NULL,
+  `largeur` varchar(255) DEFAULT NULL,
+  `hauteur` varchar(255) DEFAULT NULL,
+  `surface` varchar(255) DEFAULT NULL,
+  `volume` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_potager`),
+  KEY `fk_id_legume` (`fk_id_legume`),
+  KEY `fk_id_user` (`fk_id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `suivis` (
+  `id_suivi` int NOT NULL AUTO_INCREMENT,
+  `fk_id_legume` int DEFAULT NULL,
+  `fk_id_image` int DEFAULT NULL,
+  `date` DATETIME DEFAULT NULL,
   `numero` varchar(255) DEFAULT NULL,
   `temperature` varchar(255) DEFAULT NULL,
   `humidite` varchar(255) DEFAULT NULL,
@@ -78,10 +58,18 @@ CREATE TABLE `suivi` (
   `ec` varchar(255) DEFAULT NULL,
   `hauteur` varchar(255) DEFAULT NULL,
   `commentaire` text,
-  `active` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id_suivi`),
-  KEY `fk_id_plante` (`fk_id_plante`),
-  KEY `fk_id_image` (`fk_id_image`),
-  CONSTRAINT `suivi_ibfk_1` FOREIGN KEY (`fk_id_plante`) REFERENCES `plante` (`id_plante`),
-  CONSTRAINT `suivi_ibfk_2` FOREIGN KEY (`fk_id_image`) REFERENCES `image` (`id_image`)
+  KEY `fk_id_legume` (`fk_id_legume`),
+  KEY `fk_id_image` (`fk_id_image`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `legumes`
+  ADD CONSTRAINT `legumes_ibfk_1` FOREIGN KEY (`fk_id_image`) REFERENCES `images` (`id_image`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `potagers`
+  ADD CONSTRAINT `potagers_ibfk_1` FOREIGN KEY (`fk_id_legume`) REFERENCES `legumes` (`id_legume`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `potagers_ibfk_2` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `suivis`
+  ADD CONSTRAINT `suivis_ibfk_1` FOREIGN KEY (`fk_id_legume`) REFERENCES `legumes` (`id_legume`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `suivis_ibfk_2` FOREIGN KEY (`fk_id_image`) REFERENCES `images` (`id_image`) ON DELETE CASCADE ON UPDATE CASCADE;
